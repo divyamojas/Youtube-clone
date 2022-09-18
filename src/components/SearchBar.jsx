459e452827276177f343f61a7ca0cff09f1206d8
Myriad <divyamojas17@gmail.com>
2022-09-18 12:57:45 +0530

    Youtube clone incoming...

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { IconButton, Paper } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+
+const SearchBar = () => (
+  <Paper
+    component="form"
+    onSubmit={() => {}}
+    sx={{
+      borderRadius: 20,
+      border: "1px solid #e3e3e3",
+      pl: 2,
+      boxShadow: "none",
+      mr: { sm: 5 },
+    }}
+  >
+    <input
+      className="search-bar"
+      placeholder="Search..."
+      value=""
+      onChange={() => {}}
+    />
+    <IconButton>
+      <SearchIcon />
+    </IconButton>
+  </Paper>
+);
+
+export default SearchBar;
